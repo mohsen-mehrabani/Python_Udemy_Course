b69be07850be93819e5984d45a6b49b583c6c8d7
asus <mohsen.mehrabani@hotmail.com>
2020-03-06 23:58:01 +0330

    8:27 PM Friday, Esfand 16, 1398 AP Coordinated Universal Time (UTC)

--- GenExamples/size.py
+++ GenExamples/size.py
@@ -2,7 +2,7 @@ import sys
 
 
 def my_range(n: int):
-    print("my range starts ;-))")
+    print("my range starts ;-)")
     start = 0
     while start < n:
         print("My range is returning {}".format(start))
@@ -10,21 +10,28 @@ def my_range(n: int):
         start += 1
 
 
-_ = input("line 12")
 # big_range = range(5)
-big_range = my_range(5)
-_ = input("line 15")
-
+# big_range = my_range(5)
+big_range = range(5)
+# _ = input("line 15")
+# print(next(big_range))
 print("big_range is {} bytes".format(sys.getsizeof(big_range)))
 
 # create a list containing all the values in big_range
 big_list = []
 
-_ = input("line 22")
+# _ = input("line 22")
 for val in big_range:
-    _ = input("line 24 - inside loop")
+    # _ = input("line 24 - inside loop")
     big_list.append(val)
 
 print("big_list is {} bytes".format(sys.getsizeof(big_list)))
 print(big_range)
 print(big_list)
+
+# for i in big_range:
+#     print("i is {}".format(i))
+# for i in my_range(5):
+#     print(" i is {}".format(i))
+for i in big_range:
+    print(" i is {}".format(i))
